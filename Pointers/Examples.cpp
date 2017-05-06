7b82b35e237d5fcbe31bc2fe00d78dacd08419f9
Planmore <akarainer@yandex.ru>
2017-05-06 14:08:09 +0300

    Update Examples.cpp

--- Pointers/Examples.cpp
+++ Pointers/Examples.cpp
@@ -10,3 +10,14 @@
 	for (int i = 0; i < 5; ++i){
 		cout << "ptr + " << i << " = " << ptr + i << endl;
 	}
+// array pointer notation
+	double arr[5] = {3,4,5,2,5};
+	
+	cout << "Displaying address using pointers notation: "<< endl;
+	for (int i = 0; i < 5; i++){
+		cout << arr + i << endl;
+	}
+	cout << "Displaying values using pointers notation" << endl;
+	for (int i = 0; i < 5; i++){
+		cout << *(arr + i) << endl;
+	}
